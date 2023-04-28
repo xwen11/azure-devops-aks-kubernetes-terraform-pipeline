0d7bbd262ac03be5bc9862ca9a1301f1ab61914a
xwen11 <xwen@microsoft.com>
2023-04-28 14:02:36 -0700

    Modifying Terraform version

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -7,21 +7,21 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Central US"
+  #default = "Central US"
 }
 
 # Azure Resource Group Name
 variable "resource_group_name" {
   type = string
   description = "This variable defines the Resource Group"
-  default = "terraform-aks"
+  default = "xwen-terraform-aks"
 }
 
 # Azure AKS Environment Name
 variable "environment" {
   type = string  
   description = "This variable defines the Environment"  
-  #default = "dev2"
+  #default = "dev"
 }
 
 
