14d1224c406555b18865eb4600ceadfc3f9fb7f6
xwen11 <xwen@microsoft.com>
2023-05-01 23:14:05 -0700

    bump the version

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -9,20 +9,20 @@
 # 1. Terraform Settings Block
 terraform {
   # 1. Required Version Terraform
-  required_version = ">= 0.13"
+  required_version = "1.4.6"
   # 2. Required Terraform Providers  
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "3.54.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
-      version = "~> 2.0"
+      version = "2.38.0"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "3.5.1"
     }
   }
 
